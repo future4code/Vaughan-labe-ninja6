7211c0b58d52db817b996cd8f4e2206fb02e2272
Raquelmms <raqueldemedeirosmartins@gmail.com>
2022-01-31 14:51:40 -0300

    mudando para o app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import React from "react";
+import { AppContainer } from "./components/AppContainer";
 
-function App() {
-	return (
-        <AppContainer />
-	)
+class App extends React.Component {
+  state = {
+    currentScreem: "home",
+  };
+
+  goToClient = () => {
+    this.setState({ currentScreen: "cliente" });
+    console.log("clicou no botão para ir no cliente");
+  };
+
+  goToService = () => {
+    this.setState({ currentScreen: "prestador" });
+  };
+
+  chooseScreen = () => {
+    switch (this.state.currentScreen) {
+      case "home":
+        return <AppContainer />;
+      case "prestador":
+        return 
+      case "cliente":
+        return; //Componente cliente
+      default:
+        return <AppContainer />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        
+        <button onClick={this.goToService}>Quero ser um ninja</button>
+        <button onClick={this.goToClient}>Contratar um ninja</button>
+        {this.chooseScreen()}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import PrestadorServico from "./PrestadorServico";
 
 export class AppContainer extends Component {
 
-  state = {
-    currentScreen: "home"
-  }
-
-  goToClient = () => {
-    this.setState({currentScreen: "cliente"})
-    console.log("clicou no botão para ir no cliente")
-  }
-
-  goToService = () => {
-    this.setState({currentScreen: "prestador"})
-  }
-
-  chooseScreen = () => {
-switch(this.state.currentScreen){
-  case "home": 
-  return <AppContainer />
-  case "prestador" :
-    return <PrestadorServico/>
-    case "cliente":
-    return //Componente cliente
-    default :
-    return <AppContainer />
-}
-  }
-
-  
 
   render() {
     return (
       <div>
-        <button onClick={this.goToService}>Quero ser um ninja</button>
-        <button onClick={this.goToClient}>Contratar um ninja</button>
-
-        {this.chooseScreen()}
+        
       </div>
-    )
+    );
   }
 }
