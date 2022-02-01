a62422a52257bcd098b19de213bdea77adaeb489
jardel <jardelsantos15@gmail.com>
2022-02-01 12:36:06 -0300

    atualizacao

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-
-const MeuLogo = styled.div`
-height: 100px;
+const ConteinerHeader = styled.div`
+width: 100%;
 display:flex;
-align-items:center
-
-` 
-
-const MeuBotao = styled.div`
-width: 100px;
-display:flex;
-justify-content: space-around;
+align-items:center;
+justify-content: space-between;
+border: 1px solid black;
+margin: 0;
+background-color: grey;
 
 `
 
-export class Header extends React.Component  {
-
-
-    render() {
-      return (
-        <MeuLogo>
-          <h1>LabeNinjas</h1>
-          <MeuBotao>
-          <button onClick={this.props.goToHome} > Home</button>
+export class Header extends React.Component {
+  render() {
+    return (
+      <ConteinerHeader>
+    <div>    
+    <h1>LabeNinjas</h1>
+    </div>
+        <div>
+          <button onClick={this.props.goToHome}> Home</button>
           <button onClick={this.props.goToCart}>Carrinho</button>
-          </MeuBotao>
-        </MeuLogo>
-      );
-    }
+        </div>
+      </ConteinerHeader>
+    );
   }
-
-  
-  
+}
