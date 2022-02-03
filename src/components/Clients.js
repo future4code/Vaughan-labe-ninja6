317eea7dc5bacfa8cbcf188cf7e3f51d4cc2b276
Rodrigo Brito <rodrigo.lab@yahoo.com>
2022-02-03 15:24:32 -0300

    Estilização do filtro pronto

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,17 +1,21 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
-// import JobDetails from "./JobDetails";
+import { TextField } from "@material-ui/core";
+
+import { Box } from "@material-ui/core";
+import { InputLabel } from "@material-ui/core";
+
+import { FormControl } from "@material-ui/core";
+import { NativeSelect } from "@material-ui/core";
 
 const ContentCards = styled.div`
 display: grid;
 grid-template-columns: 1fr 1fr 1fr 1fr;
 row-gap: 3rem;
 width: 100%;
-justify-items: center;
-
-
-`
+justify-items: center;0
+`;
 
 const JobCards = styled.div`
   display: flex;
@@ -27,29 +31,26 @@ const JobCards = styled.div`
   box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
   cursor: pointer;
   -ms-word-break: break-all;
-    word-break: break-all;
+  word-break: break-all;
 
   &:hover {
     background-color: #e5e5e5;
   }
 `;
 
-
-
-
 const FilterContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
-  width: 95%;
+  width: 100%;
   height: 100%;
-  border: 1px solid black;
+  border: none;
   border-radius: 5px;
   margin: 10px;
   padding: 10px;
-  background-color: #f5f5f5;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  background-color: #f5f4fc;
+  /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
 `;
 
 export default class Clients extends React.Component {
@@ -85,8 +86,7 @@ export default class Clients extends React.Component {
         console.log(error);
       });
   };
-
-  onChangeAtualizacaoDaBusca = (event) => {
+  PonChangeAtualizacaoDaBusca = (event) => {
     this.setState({
       query: event.target.value,
     });
@@ -116,98 +116,104 @@ export default class Clients extends React.Component {
     });
   };
 
- 
-  
-
   render() {
     return (
       <div>
-        
         <FilterContainer>
-          <h3>Busca por nome</h3>
-          <input
+          <TextField
+            id="outlined-basic"
+            label="Filtrar por Titilo"
+            variant="outlined"
             placeholder="Busca por titulo"
             value={this.state.query}
             onChange={this.onChangeAtualizacaoDaBusca}
           />
 
-          <h3>Valor mínimo</h3>
-          <input
+          <TextField
+            id="outlined-basic"
+            label="Filtrar por Valor Mínimo"
+            variant="outlined"
             type="number"
             placeholder="  $"
             value={this.state.minValue}
             onChange={this.onChangeAtualizacaoDoMinimo}
           />
 
-          <h3>Valor máximo</h3>
-          <input
+          <TextField
+            id="outlined-basic"
+            label="Filtrar por Valor Máximo"
+            variant="outlined"
             type="number"
             placeholder="  $$$$"
             value={this.state.maxValue}
             onChange={this.onChangeAtualizacaoDoMaximo}
           />
 
-          <select
-            value={this.state.ordem}
-            onChange={this.onChangeAtualizacaoDaOrdem}
-          >
-            <option>Sem Ordenação</option>
-            <option>Menor Valor</option>
-            <option>Maior Valor</option>
-            <option>Título</option>
-            <option>Prazo</option>
-          </select>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Ordenar por:
+              </InputLabel>
+              <NativeSelect
+                defaultValue={""}
+                value={this.state.ordem}
+                onChange={this.onChangeAtualizacaoDaOrdem}
+              >
+                <option> Sem Ordenação </option>
+                <option> Menor Valor </option>
+                <option> Maior Valor </option>
+                <option> Título </option>
+                <option> Prazo </option>
+              </NativeSelect>
+            </FormControl>
+          </Box>
         </FilterContainer>
 
         <h1>Lista de Jobs</h1>
 
         <ContentCards>
-        
-        {this.state.jobs
-          .filter((job) => {
-            return job.title
-              .toLowerCase()
-              .includes(this.state.query.toLowerCase());
-          })
-          .filter((job) => {
-            return (
-              this.state.minValue === "" || job.price >= this.state.minValue
-            );
-          })
-          .filter((job) => {
-            return (
-              this.state.maxValue === "" || job.price <= this.state.maxValue
-            );
-          })
-          .sort((a, b) => {
-            switch (this.state.ordem) {
-              case "Menor Valor":
-                return a.price - b.price;
-              case "Maior Valor":
-                return b.price - a.price;
-              case "Título":
-                return a.title.localeCompare(b.title);
-              case "Prazo":
-                return a.dueDate.localeCompare(b.dueDate);
-            }
-          })
-          .map((job) => {
-            return (
-              
-              <JobCards>
-                <h1>{job.title}</h1>
-                <p>{job.description}</p>
-                <p>{job.price}</p>
-                <p>{job.dueDate}</p>
-                <button onClick={() => this.props.addJobToCart(job)}>
-                  Adicionar ao Carrinho
-                </button>
-                
-              </JobCards>
-              
-            );
-          })}
-</ContentCards>
+          {this.state.jobs
+            .filter((job) => {
+              return job.title
+                .toLowerCase()
+                .includes(this.state.query.toLowerCase());
+            })
+            .filter((job) => {
+              return (
+                this.state.minValue === "" || job.price >= this.state.minValue
+              );
+            })
+            .filter((job) => {
+              return (
+                this.state.maxValue === "" || job.price <= this.state.maxValue
+              );
+            })
+            .sort((a, b) => {
+              switch (this.state.ordem) {
+                case "Menor Valor":
+                  return a.price - b.price;
+                case "Maior Valor":
+                  return b.price - a.price;
+                case "Título":
+                  return a.title.localeCompare(b.title);
+                case "Prazo":
+                  return a.dueDate.localeCompare(b.dueDate);
+              }
+            })
+            .map((job) => {
+              return (
+                <JobCards>
+                  <h1>{job.title}</h1>
+                  <p>{job.description}</p>
+                  <p>{job.price}</p>
+                  <p>{job.dueDate}</p>
+                  <button onClick={() => this.props.addJobToCart(job)}>
+                    Adicionar ao Carrinho
+                  </button>
+                </JobCards>
+              );
+            })}
+        </ContentCards>
         <button onClick={this.getAllJobs}>Atualizar</button>
       </div>
     );
